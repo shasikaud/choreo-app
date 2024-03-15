4de97c72baddd4073aea69a196666df008bfe4bd
Shasika Udayanga <shasikaudayanga@gmail.com>
2024-03-15 11:57:28 +0000

    changed favicon

--- reading-list-front-end-with-managed-auth/src/index.tsx
+++ reading-list-front-end-with-managed-auth/src/index.tsx
@@ -1,19 +1,3 @@
-// Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
-
-// WSO2 LLC. licenses this file to you under the Apache License,
-// Version 2.0 (the "License"); you may not use this file except
-// in compliance with the License.
-// You may obtain a copy of the License at
-
-//    http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
 import Cookies from 'js-cookie';
 import React, { useEffect, useState } from "react";
 import { Tab } from "@headlessui/react";
@@ -108,12 +92,27 @@ export default function App() {
 
   if (!signedIn) {
     return (
-      <button
-        className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
-        onClick={() => { window.location.href = "/auth/login" }}
-      >
-        Login
-      </button>
+      <div className='flex flex-col gap-4'>
+        <button
+          className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
+          onClick={() => { window.location.href = "/auth/login" }}
+        >
+          Login
+        </button>
+        <button
+          className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
+          onClick={() => { window.location.href = "/auth/login" }}
+        >
+          Register
+        </button>
+        <button
+          className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
+          onClick={() => { window.location.href = "/auth/login" }}
+        >
+          Login as a Guest
+        </button>
+      </div>
+      
     );
   }
 
